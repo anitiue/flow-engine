3701a5fcc02253054626590a600ca336868e7747
anitiue <anitiue@gmail.com>
2026-08-14 00:12:56 +0800

    Initial commit: Flow Engine v2.1.0

---
+++ tools/flow_archive.js
@@ -0,0 +1,6 @@
+import { getDefinition } from "./flow.js";
+const tool = getDefinition("flow_archive");
+export const name = "flow_archive";
+export const description = tool.description;
+export const parameters = tool.parameters;
+export const execute = tool.execute;
